108c570f48aa7fdc52d40c2cc7a2969f0d5d105a
Estefanía Fdez Muñoz <estefafdez@gmail.com>
2021-01-14 18:00:13 +0100

    example of unit tests with jasmine in js

---
+++ spec/Calculator.js
@@ -0,0 +1,14 @@
+var Calculator = {
+    suma: function (num1, num2) {
+        return num1 + num2;
+    },
+    resta: function (num1, num2) {
+        return num1 - num2;
+    },
+    multiplicacion: function (num1, num2) {
+        return num1 * num2;
+    },
+    division: function (num1, num2) {
+        return num2 && num1/num2;
+    }
+};
---
+++ spec/test.spec.js
@@ -0,0 +1,22 @@
+describe('Unit test with a simple calculator and Jasmine', function () {     
+
+    // Generate a random number each time that the tests are executed
+    var number1 = Math.floor(Math.random() * 10);
+    var number2 = Math.floor(Math.random() * 10);
+
+    it('should add ' + number1 + ' plus ' +number2 + ' and be equals to ' + (number1 + number2), function () {
+        expect(Calculator.suma(number1, number2)).toBe(number1 + number2);
+    });
+
+    it('should subtract ' + number1 + ' from ' +number2 + ' and be equals to ' + (number1 - number2), function () {
+        expect(Calculator.resta(number1, number2)).toBe(number1 - number2);
+    });
+
+    it('should multiply ' + number1 + 'x' + number2 + ' and be equals to ' + (number1*number2), function () {
+       expect(Calculator.multiplicacion(number1, number2)).toBe(number1 * number2);
+    });
+
+    it('should divide ' + number1 + '/' + number2 + ' and be equals to ' + (number2 && number1/number2), function () {
+        expect(Calculator.division(number1, number2)).toBe(number2 && number1/number2);
+    });
+});
